e36a654d982bd343df5857d767ad248e2d30d8dc
DarioLoForti <lofortidario@gmail.com>
2024-03-21 16:44:00 +0100

    Add file config

---
+++ config/doctors.php
@@ -0,0 +1,174 @@
+<?php
+
+return [
+    [
+        "phone" => "123456789",
+        "profile_image_url" => "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?
+    q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Roma",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "987654321",
+        "profile_image_url" => "https://images.unsplash.com/photo-1594824476967-48c8b964273f?
+    q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Milano",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "345678912",
+        "profile_image_url" => "https://images.unsplash.com/photo-1537368910025-700350fe46c7?
+    q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Napoli",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "789123456",
+        "profile_image_url" => "https://images.unsplash.com/photo-1559839734-2b71ea197ec2?
+    q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D
+    ",
+        "city" => "Torino",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "234567891",
+        "profile_image_url" => "https://plus.unsplash.com/
+    premium_photo-1661764878654-3d0fc2eefcca?
+    q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Palermo",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "678912345",
+        "profile_image_url" => "
+    https://images.unsplash.com/photo-1643297654416-05795d62e39c?
+    q=80&w=2906&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Genova",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "456789123",
+        "profile_image_url" => "https://images.unsplash.com/
+    photo-1622253692010-333f2da6031d?
+    q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Bologna",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "891234567",
+        "profile_image_url" => "
+    https://plus.unsplash.com/premium_photo-1681966907271-1e350ec3bb95?
+    q=80&w=2986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Firenze",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "567891234",
+        "profile_image_url" => "https://plus.unsplash.com/
+    premium_photo-1658506671316-0b293df7c72b?
+    q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Bari",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "912345678",
+        "profile_image_url" => "https://www.fortuneita.com/wp-content/uploads/2023/04/
+    shutterstock_1814470595.jpg
+    ",
+        "city" => "Catania",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "678912345",
+        "profile_image_url" => "https://plus.unsplash.com/premium_photo-1681843036167-
+    c7a696e3dfcb?
+    q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D
+    ",
+        "city" => "Venezia",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "345678912",
+        "profile_image_url" => "
+    https://st2.depositphotos.com/3889193/7657/i/450/depositphotos_76570869-stock-photoconfident-
+    female-doctor-posing-in.jpg",
+        "city" => "Verona",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "789123456",
+        "profile_image_url" => "
+    https://images.unsplash.com/photo-1580281658626-ee379f3cce93?
+    q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Messina",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "234567891",
+        "profile_image_url" => "https://img.freepik.com/premium-photo/pretty-doctor-happy-behospital_
+    23-2148728330.jpg
+    ",
+        "city" => "Padova",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "678912345",
+        "profile_image_url" => "
+    https://plus.unsplash.com/premium_photo-1682141125356-9ebba1fa94ea?
+    q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D",
+        "city" => "Trieste",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "345678912",
+        "profile_image_url" => "
+    https://static6.depositphotos.com/1000816/608/i/950/depositphotos_6087460-stock-photomedical-
+    doctor-woman-in-the.jpg",
+        "city" => "Taranto",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "789123456",
+        "profile_image_url" => "https://images.unsplash.com/
+    photo-1582895361887-24daa40c8667?
+    q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlf
+    Hx8fGVufDB8fHx8fA%3D%3D
+    ",
+        "city" => "Brescia",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "234567891",
+        "profile_image_url" => "https://img.freepik.com/premium-photo/portrait-confident-youngfemale-
+    doctor-white-background-ai-generated_943087-2803.jpg",
+        "city" => "Reggio Calabria",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "678912345",
+        "profile_image_url" => "https://st2.depositphotos.com/1743476/5738/i/450/depositphotos_57385697-stock-photo-confident-mature-doctor.jpg",
+        "city" => "Modena",
+        "curriculum" => "Filepdf",
+    ],
+    [
+        "phone" => "345678912",
+        "profile_image_url" => "
+    https://media.istockphoto.com/id/1372002650/it/foto/ritratto-ritagliato-di-unattraente-giovanedottoressa-
+    in-piedi-con-le-braccia-conserte-in.jpg?s=612x612&w=0&k=20&c=1ERjOl5ORbkvnNAm1pM4KUs18FUDu2nv3QG-
+    j6paDs=",
+        "city" => "Parma",
+        "curriculum" => "Filepdf",
+    ]
+];
---
+++ config/user.php
@@ -0,0 +1,144 @@
+<?php
+
+return [
+    [
+        "name" => 'Paolo',
+        'surname' => 'Rossi',
+        'email' => 'paolorossi@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Roma 12'
+    ],
+    [
+        "name" => 'Francesco',
+        'surname' => 'Sassi',
+        'email' => 'francosassi@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Corso Italia 45'
+    ],
+    [
+        "name" => 'Maria',
+        'surname' => 'Bianchi',
+        'email' => 'mariabianchi@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Dante 56'
+    ],
+    [
+        "name" => 'Luca',
+        'surname' => 'Romano',
+        'email' => 'lucaromano@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Vicolo delle Rose 7'
+    ],
+    [
+        "name" => 'Anna',
+        'surname' => 'Ricci',
+        'email' => 'annaricci@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Vittorio Emanuele 31'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Rossi',
+        'email' => 'marcorossi@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Leopardi 8'
+    ],
+    [
+        "name" => 'Giulia',
+        'surname' => 'Moretti',
+        'email' => 'giuliamoretti@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Vicolo dei Fiori 15'
+    ],
+    [
+        "name" => 'Simone',
+        'surname' => 'Ferrari',
+        'email' => 'simoneferrari@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Verdi 24'
+    ],
+    [
+        "name" => 'Chiara',
+        'surname' => 'Galli',
+        'email' => 'chiaragalli@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Corso Matteotti 37'
+    ],
+    [
+        "name" => 'Andrea',
+        'surname' => 'Conti',
+        'email' => 'andreaconti@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via dei Mille 50'
+    ],
+    [
+        "name" => 'Martina',
+        'surname' => 'Barbieri',
+        'email' => 'martinabarbieri@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Viale Kennedy 3'
+    ],
+    [
+        "name" => 'Alessandro',
+        'surname' => 'Vitali',
+        'email' => 'alessandrovitali@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Carducci 18'
+    ],
+    [
+        "name" => 'Elisa',
+        'surname' => 'Pellegrini',
+        'email' => 'elisapellegrini@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Vicolo della Pace 22'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Marchetti',
+        'email' => 'davidemarchetti@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Rossini 29'
+    ],
+    [
+        "name" => 'Silvia',
+        'surname' => 'Fabbri',
+        'email' => 'silviafabbri@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Corso Cavour 33'
+    ],
+    [
+        "name" => 'Mattia',
+        'surname' => 'Gentile',
+        'email' => 'mattiagentile@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Manzoni 40'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Lombardi',
+        'email' => 'saralombardi@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Piazza Kennedy 11'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Mancini',
+        'email' => 'marcomancini@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via Montegrappa 6'
+    ],
+    [
+        "name" => 'Elena',
+        'surname' => 'Rinaldi',
+        'email' => 'elenarinaldi@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Vicolo dei Sogni 19'
+    ],
+    [
+        "name" => 'Fabio',
+        'surname' => 'Marini',
+        'email' => 'fabiomarini@gmail.com',
+        'password' => 'BDoctors',
+        'address' => 'Via della Repubblica 26'
+    ]
+];
