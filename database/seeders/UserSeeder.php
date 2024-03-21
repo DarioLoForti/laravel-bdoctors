6aa6d9a5c7a37187695dd29701da80d445fa5059
Zariy7 <nazariy.hereha@gmail.com>
2024-03-21 17:41:51 +0100

    user+doctor seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = config('user');
+
+        foreach($users as $user) {
+            $newUser = new User();
+            $newUser->name = $user['name'];
+            $newUser->surname = $user['surname'];
+            $newUser->email = $user['email'];
+            $newUser->password = $user['password'];
+            $newUser->address = $user['address'];
+            $newUser->save();
+        }
+    }
+}
