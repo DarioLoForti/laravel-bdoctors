9d2bb250372e0f6ac091de2bca0e22acffc1703b
DarioLoForti <lofortidario@gmail.com>
2024-03-21 16:32:13 +0100

    first commit

---
+++ database/migrations/2024_03_21_152120_create_doctors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('city', 150);
+            $table->bigInteger('phone');
+            $table->text('image')->nullable();
+            $table->string('cv', 150);
+            $table->text('services');
+            $table->string('slug', 150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
