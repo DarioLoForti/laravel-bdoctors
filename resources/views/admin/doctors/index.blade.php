b0cea7364b3cc511e7605da157be701da2eec152
Zariy7 <nazariy.hereha@gmail.com>
2024-03-25 15:36:29 +0100

    proposal for show/index page look

--- resources/views/admin/doctors/index.blade.php
+++ resources/views/admin/doctors/index.blade.php
@@ -2,11 +2,13 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="container my-3 bg-white rounded">
+                <div class="row border border-black mb-3 rounded">
 
-            <div class="col-12">
-                <div class="row">
-                    <div class="col-4 my-5 ">
-                        <div class="d-flex justify-content-start pt-2 ">
+                    {{-- UPDATE & DELETE BUTTONS --}}
+
+                    <div class="col-4 my-5">
+                        <div class="d-flex justify-content-start pt-2">
                             <a href="{{ url('admin') }}" class=""><button class="btn btn-square btn-primary "><i
                                         class="fa-solid fa-hand-point-left" style="color: #ffffff;"></i></button></a>
                             <a href="{{ route('profile.edit') }}" class="mx-2"><button
@@ -18,55 +20,86 @@
                             @include('admin.doctors.modal')
                         </div>
                     </div>
+
+                    {{-- PAGE TITLE --}}
+
                     <div class="col-8 my-5">
-                        <h1 class="">Profilo Personale {{ $doctor->user->name }} {{ $doctor->user->surname }}</h1>
+                        <h1 class="">Dr. {{ $doctor->user->name }} {{ $doctor->user->surname }}</h1>
                     </div>
                 </div>
 
-                <div class="row">
+                <div class="row mb-3">
                     <div class="col-6">
-                        <div class="image">
+                        <div class="image bg-warning d-flex justify-content-center rounded">
                             <img class="dettaglio-immagine" src="{{ $doctor->image }}">
                         </div>
-                        <div class="services mt-5">
-
+                        @if(count($doctor->specializations) != 0)
+                        <div class="services my-2">
                             <div><strong>Specializzazioni:</strong> <br>
                                 @foreach ($doctor->specializations as $specialization)
                                     {{ $specialization->name }} <br>
                                 @endforeach
                             </div>
                         </div>
-                        <div class="services mt-5">
-                            <div><strong>Prestazioni:</strong> <br>{{ $doctor->services }}</div>
-                        </div>
+                        @endif
                         <a href="{{ url('admin') }}" class="mx-2"><button class="btn btn-square btn-primary mt-4"><i
-                                    class="fa-solid fa-envelope" style="color: #ffffff;"></i></button></a>
-                    </div>
-                    <div class="col-6">
-                        <div>
-                            <div><strong>Email:</strong> {{ $doctor->user->email }}</div><br>
-                            <div><strong>Numero di telefono:</strong> {{ $doctor->phone }}</div><br>
-                            <div><strong>Città:</strong> {{ $doctor->city }}</div><br>
-                            <div><strong>Indirizzo:</strong> {{ $doctor->user->address }}</div><br>
-                            <div><strong>Curriculum:</strong><br>
-                                <img class="dettaglio-immagine" src="{{ $doctor->cv }}">
+                            class="fa-solid fa-envelope" style="color: #ffffff;"></i></button></a>
+                        </div>
+                        <div class="col-6">
+                            <div>
+                                <div class="d-flex justify-content-between my-2">
+                                    <span>
+                                        <strong>Email:</strong>
+                                    </span>
+                                    <div>{{ $doctor->user->email }}</div>
+                                </div>
+                                <div class="d-flex justify-content-between my-2">
+                                    <span>
+                                        <strong>Numero di Telefono:</strong>
+                                    </span>
+                                    <div>{{ $doctor->phone }}</div>
+                                </div>
+                                <div class="d-flex justify-content-between my-2">
+                                    <span>
+                                        <strong>Città:</strong>
+                                    </span>
+                                    <div>{{ $doctor->city }}</div>
+                                </div>
+                                <div class="d-flex justify-content-between my-2">
+                                    <span>
+                                        <strong>Indirizzo:</strong>
+                                    </span>
+                                    <div>{{ $doctor->user->address }}</div>
+                                </div>
+                                <div class="services my-2">
+                                    <div>
+                                        <strong>Prestazioni:</strong>
+                                        <div>
+                                            {{ $doctor->services }}
+                                        </div>
+                                    </div>
+                                </div>
+                                @if($doctor->cv != 'Filepdf')
+                                <div><strong>Curriculum:</strong><br>
+                                    <img class="dettaglio-immagine" src="{{ $doctor->cv }}">
+                                </div>
+                                @endif
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 ">
-                {{-- <div class="d-flex justify-content-center mt-2">
-                    <a href="{{ url('admin') }}" class=""><button class="btn btn-square btn-primary mt-4"><i
-                                class="fa-solid fa-hand-point-left" style="color: #ffffff;"></i></button></a>
-                    <a href="{{ route('profile.edit', ['doctor' => $doctor->id]) }}" class="mx-2"><button
-                            class="btn btn-square btn-warning mt-4"><i class="fa-regular fa-pen-to-square"
-                                style="color: #ffffff;"></i></button></a>
-                    <button class="btn btn-square btn-danger mt-4" data-bs-toggle="modal"
-                        data-bs-target="#delete-account"><i class="fa-solid fa-trash" style="color:#ffffff;"></i></button>
-                    @include('admin.doctors.modal')
-                </div> --}}
+                <div class="col-12">
+                    {{-- <div class="d-flex justify-content-center mt-2">
+                        <a href="{{ url('admin') }}" class=""><button class="btn btn-square btn-primary mt-4"><i
+                                    class="fa-solid fa-hand-point-left" style="color: #ffffff;"></i></button></a>
+                        <a href="{{ route('profile.edit', ['doctor' => $doctor->id]) }}" class="mx-2"><button
+                                class="btn btn-square btn-warning mt-4"><i class="fa-regular fa-pen-to-square"
+                                    style="color: #ffffff;"></i></button></a>
+                        <button class="btn btn-square btn-danger mt-4" data-bs-toggle="modal"
+                            data-bs-target="#delete-account"><i class="fa-solid fa-trash" style="color:#ffffff;"></i></button>
+                        @include('admin.doctors.modal')
+                    </div> --}}
+                </div>
             </div>
         </div>
-    </div>
 @endsection
