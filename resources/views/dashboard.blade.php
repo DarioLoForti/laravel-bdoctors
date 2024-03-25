da4228e25ad909f0f7fdcf4ff2e2fa6cb5f20248
Zariy7 <nazariy.hereha@gmail.com>
2024-03-25 08:12:46 +0100

    user menus moved in a more sensible manner

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,11 +17,20 @@
                                     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
                                         id="menu">
                                         <li class="nav-item">
-
                                             <a class="nav-link align-middle px-0 text-white" href="{{ url('/') }}"><i
                                                     class="fa-solid fa-house me-2"
                                                     style="color: #ffffff;"></i>{{ __('Home') }}
-
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="{{ route('admin.doctors.index') }}" class="nav-link px-0">
+                                                <i class="fa-solid fa-user-doctor" style="color: #ffffff;"></i>
+                                                <span class="ms-1 d-none d-sm-inline text-white">Profile</span>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <i class="fa-solid fa-message" style="color: #ffffff;"></i>
+                                                <span class="ms-1 d-none d-sm-inline text-white">Messages</span>
                                             </a>
                                         </li>
                                         <li>
@@ -34,9 +43,13 @@
                                             <ul class="collapse show nav flex-column ms-1" id="submenu1"
                                                 data-bs-parent="#menu">
                                                 <li class="w-100">
-                                                    <a href="{{ route('admin.doctors.index') }}" class="nav-link px-0">
-                                                        <i class="fa-solid fa-user-doctor" style="color: #ffffff;"></i>
-                                                        <span class="ms-1 d-none d-sm-inline text-white">Profilo</span></a>
+                                                    Reviews
+                                                </li>
+                                                <li class="w-100">
+                                                    Ratings
+                                                </li>
+                                                <li class="w-100">
+                                                    Statistics
                                                 </li>
                                             </ul>
                                         </li>
@@ -54,19 +67,44 @@
                         </h2>
                         <div class="row justify-content-center">
                             <div class="col-8">
-                                <h1 class="text-mygreen">Benvenuto in BDoctors
-                                    {{ Auth::user()->name }} {{ Auth::user()->surname }}</h1>
+                                <h1 class="text-mygreen">Benvenuto in BDoctors, Dr.
+                                    {{ Auth::user()->name }} {{ Auth::user()->surname }}. </h1>
+                            </div>
+                            <div class="row my-3">
+                                @if(count(Auth::user()->doctor->specializations) == 0)
+                                <div class="col-12 bg-success p-3 rounded d-flex justify-content-between align-items-center">
+                                    <div class="text-white">
+                                        Your profile has not set any specializations. Edit your profile to better match patient's search results!
+                                    </div>
+                                    <div class="btn btn-small btn-primary">
+                                        <a href="{{ route('profile.edit', Auth::user()->doctor->id) }}" class="text-white">
+                                            Add Specializations
+                                        </a>
+                                    </div>
+                                </div>
+                                @endif
+                            </div>
+                            <div class="row my-3">
+                                @if(Auth::user()->doctor->sponsorship == null)
+                                    <div class="col-12 bg-success p-3 rounded d-flex justify-content-between align-items-center">
+                                        <div class="text-white">
+                                            You don't have a sponsorship. Buy one to reach more clients, faster!
+                                        </div>
+                                        <div class="btn btn-small btn-primary">
+                                            See Our Offers
+                                        </div>
+                                    </div>
+                                @endif
                             </div>
                             <div class="row">
-                                @if (Auth::user()->doctor)
+{{--                                 @if (Auth::user()->doctor)
                                     <div class="col-12">
                                         <div class="jumbo">
                                             <img class="jumbo-img" src="{{ Auth::user()->doctor->image }}">
                                         </div>
                                     </div>
-                                @endif
+                                @endif --}}
                             </div>
-
                         </div>
                     </div>
                 </div>
