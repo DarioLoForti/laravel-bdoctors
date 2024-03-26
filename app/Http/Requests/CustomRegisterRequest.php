876bcfb485040b99f9677180ca731813dff2f3bf
Zariy7 <nazariy.hereha@gmail.com>
2024-03-26 11:12:45 +0100

    request updated

--- app/Http/Requests/CustomRegisterRequest.php
+++ app/Http/Requests/CustomRegisterRequest.php
@@ -28,12 +28,12 @@ class CustomRegisterRequest extends FormRequest
             'surname' => 'required|string|max:50',
             'address' => 'required|string|max:150',
             'email' => 'required|string|email|max:255',
-            'password' => 'required|confirmed',/* Rules\Password::defaults(), */
+            'password' => 'required',/* Rules\Password::defaults(), */
             'city' => 'required|max:150',
             'phone' => 'nullable|numeric',
             'image' => 'nullable|image',
             'cv' => 'nullable|mimes:pdf,docx',
-            'services' => 'nullable|max:300',
+            'services' => 'nullable',
         ];
     }
     public function messages()
@@ -41,19 +41,23 @@ class CustomRegisterRequest extends FormRequest
         return [
             'name.required' => 'Il nome è obbligatorio.',
             'name.string' => 'Il nome dev\'essere una stringa.',
+            'name.max' => 'Il nome dev\'essere al massimo di 50 caratteri.',
             'surname.required' => 'Il cognome è obbligatorio.',
             'surname.string' => 'Il cognome dev\'essere una stringa.',
+            'surname.max' => 'Il cognome dev\'essere al massimo di 50 caratteri.',
             'address.required' => 'L\'indirizzo è obbligatorio.',
             'address.string' => 'L\'indirizzo dev\'essere una stringa.',
+            'address.max' => 'L\'indirizzo dev\'essere al massimo di 150 caratteri.',
             'email.required' => 'La mail è obbligatoria.',
             'email.string' => 'La mail dev\'essere una stringa.',
+            'email.max' => 'La mail dev\'essere al massimo di 255 caratteri.',
+            'email.email' => 'La mail dev\'essere una mail! (Contenere un @ e un .).',
             'password.required' => 'Devi inserire una passoword!',
             'city.required' => 'La città è obbligatoria.',
             'city.max' => 'La lunghezza del campo non deve superare i 150 caratteri.',
             'phone.numeric' => 'Questo campo accetta solo valori numerici.',
             'image.image' => 'Puoi inserire solo un file di tipo immagine.',
             'cv.mimes' => 'Puoi inserire solo file di tipo:.pdf , .docx.',
-            'services.max' => 'La lunghezza del campo non deve superare i 300 caratteri.'
         ];
     }
 }
