9a0c684ca5efc04b4c00bd0cfdecf6c4ca67159b
Zariy7 <nazariy.hereha@gmail.com>
2024-03-27 03:36:05 +0100

    API call filtering perfected

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -11,11 +11,48 @@ class DoctorController extends Controller
 {
     public function index()
     {
-        $doctors = Doctor::with('user')->get();
+        $doctors = [];
+
+        /* 
+        CONTROLLA PRIMA SE NELLA REQUEST E' STATA INVIATA UNA CITTA'.
+        SE UNA CITTA' E' STATA RICHIESTA, 
+        LA QUERY AL DATABASE PRENDERA' SOLO I MEDICI CON IL VALORE CITTA' SIMILE ALLA RICHIESTA.
+        ALTRIMENTI, VERRANNO PRESI TUTTI I MEDICI DEL DATABASE.
+        */
+
+        if(isset($_REQUEST['city']) && $_REQUEST['city'] != ''){
+            $doctors = Doctor::with('user')->with('specializations')->where('city', 'like', '%'.$_REQUEST['city'].'%')->get();
+        }
+        else{
+            $doctors = Doctor::with('user')->with('specializations')->get();
+        }
+
+
+        $filteredDoctors = [];
+
+        /* 
+        CONTROLLA PRIMA SE NELLA REQUEST E' STATA INVIATA UNA SPECIALIZZAZIONE.
+        CONTROLLA OGNI SPECIALIZZAZIONE DI OGNI MEDICO PRESO IN PRECEDENZA.
+        SE UNA DELLE SPECIALIZZAZIONI DEL MEDICO COMBACIA CON QUELLA INVIATA PER REQUEST, 
+        IL MEDICO VIENE SALVATO NEL NUOVO ARRAY. 
+        */
+
+        if(isset($_REQUEST['specialization']) && $_REQUEST['specialization'] != ''){
+            foreach ($doctors as $doctor){
+                foreach ($doctor->specializations as $specialization) {
+                    if(str_contains($specialization->slug, $_REQUEST['specialization'])){
+                        array_push($filteredDoctors, $doctor);
+                    }
+                }
+            }
+        }
+        else{
+            $filteredDoctors = $doctors;
+        }
 
         return response()->json([
             'success' => true,
-            'results' => $doctors
+            'results' => $filteredDoctors
         ]);
     }
 
