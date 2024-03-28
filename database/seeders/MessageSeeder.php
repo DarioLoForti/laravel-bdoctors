b9cedddfc91efc41cad69437d9b6a0e8a490ba1a
Zariy7 <nazariy.hereha@gmail.com>
2024-03-28 11:54:48 +0100

    seeder messages reviews

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Message;
+use App\Models\Doctor;
 
 class MessageSeeder extends Seeder
 {
@@ -16,13 +18,18 @@ class MessageSeeder extends Seeder
     public function run()
     {
         $messages = config('message');
+        $minDoc = DB::table('doctors')->min('id');
+        $maxDoc = DB::table('doctors')->max('id');
+
 
         foreach ($messages as $message) {
-            $new_message = new message();
+            $new_message = new Message();
 
+            $new_message->doctor_id = rand($minDoc, $maxDoc);
             $new_message->name = $message['name'];
             $new_message->text = $message['text'];
             $new_message->email = $message['email'];
+
             $new_message->save();
         }
     }
--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Review;
+use App\Models\Doctor;
 
 class ReviewSeeder extends Seeder
 {
@@ -16,13 +18,17 @@ class ReviewSeeder extends Seeder
     public function run()
     {
         $reviews = config('review');
+        $minDoc = DB::table('doctors')->min('id');
+        $maxDoc = DB::table('doctors')->max('id');
 
         foreach ($reviews as $review) {
             $new_review = new review();
 
+            $new_review->doctor_id = rand($minDoc, $maxDoc);
             $new_review->name = $review['name'];
             $new_review->text = $review['text'];
             $new_review->email = $review['email'];
+
             $new_review->save();
         }
     }
