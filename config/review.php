c8fdcdd9a5cc0f59f978eb9ecfa8bc152e51932a
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-03-28 11:32:08 +0100

    as

---
+++ config/review.php
@@ -0,0 +1,73 @@
+<?php
+
+return [
+    [
+        "name" => "Mario Filippi",
+        "email" => "mario@mail.it",
+        "text" => "Dott.molto in gamba..ascolta con attenzione e ha svolto su di me una visita accurata,spero di risolvere il mio problema, lo consiglio vivamente",
+    ],
+    [
+        "name" => "Simona Andrei",
+        "email" => "simona@mail.it",
+        "text" => "Per fortuna non avevo quello che pensavo ,il dottore mi ha tranquillizzato,spiegato tutto con calma ma la cosa che mi è piaciuta di più è che si è preso il tempo giusto per spiegarmi tutto e rispondere a tutti i miei dubbi senza badare all orologio.",
+    ],
+    [
+        "name" => "Paola Guarneri",
+        "email" => "paola@mail.it",
+        "text" => "Mi sono trovato molto bene. Dottore Mazzola mi ha spiegato tutto con la chiarezza e mi ha fornito una cura che ha funzionato per trattare i miei sintomi. Molto accogliente e gentile, attento in ascolto.
+
+        ",
+    ],
+    [
+        "name" => "Michela Gioia",
+        "email" => "michela@mail.it",
+        "text" => "professionista preciso, scrupoloso e paziente, la visita è stata completa e tutti i passaggi, dalla diagnosi fino alla terapia, sono stati spiegati con calma ed empatia verso il paziente.
+
+        ",
+    ],
+    [
+        "name" => "Marco Palermo",
+        "email" => "marco@mail.it",
+        "text" => "Ottima visita. L'unico cosa che potrebbero migliorare è il costo per me eccessivo",
+    ],
+    [
+        "name" => "Luca Trovato",
+        "email" => "luca@mail.it",
+        "text" => "Dottore molto competente ha spiegato tutto con molta semplicità. Ha fatto una visita molto accurata. Mi ha colpito molto favorevolmente il fatto che si fosse informato preventivamente dei motivi della mia visita leggendo il post della prenotazione. Lo consiglio per chi vuole essere visitato da un professionista serio e competente.",
+    ],
+    [
+        "name" => "Simone Coniglio",
+        "email" => "simone@mail.it",
+        "text" => "La professionalità in persona! Serio schietto e disponibile, molto esaustivo nelle domande che gli vengono poste. Grazie mille a presto",
+    ],
+    [
+        "name" => "Vittorio Dibenedetto",
+        "email" => "vittorio@mail.it",
+        "text" => "Oggi ho avuto il piacere di conoscere ed essere visitato da un Dottore giovane competente, empatico e di una grande professionalità. Lo consiglio a chiunque cerchi un serio professionista.",
+    ],
+    [
+        "name" => "Giulia Pletto",
+        "email" => "giulia@mail.it",
+        "text" => "Si è dimostrato molto cortese e molto professionale, durante la visita mi ha spiegato tutto in modo molto dettagliato. Lo consiglio vivamente.",
+    ],
+    [
+        "name" => "Federica Marino",
+        "email" => "federica@mail.it",
+        "text" => "Il dottore è stato molto attento e scrupoloso durante la visita e ha individuato la diagnosi giusta, a differenza di altri dottori consultati in precedenza. Lo ringrazio.",
+    ],
+    [
+        "name" => "Mattia Insalaco",
+        "email" => "mattia@mail.it",
+        "text" => "Dottore competente e gentile. Ha fatto a mia figlia una visita molto accurata, dando spiegazione chiara e dettagliata del suo problema. Lo consiglio vivamente.",
+    ],
+    [
+        "name" => "Simone Lo Piano",
+        "email" => "simone@mail.it",
+        "text" => "È la prima volta che effettuo una visita cosi approfondita come quella che mi ha fatto il dottore che è stato molto preciso sia nella spiegazione degli esami che stava eseguendo sia nella spiegazione della patologia riscontrata. Fa piacere incontrare giovani professionisti con questo livello di accuratezza e competenza.",
+    ],
+    [
+        "name" => "Giuseppe Lomonaco",
+        "email" => "giuseppe@mail.it",
+        "text" => "Medico attento e disponibile a fornirmi tutti i chiarimenti richiesti. Sono rimasta molto soddisfatta anche per l'empatia dimostrata. Lo consiglio.",
+    ]
+];
