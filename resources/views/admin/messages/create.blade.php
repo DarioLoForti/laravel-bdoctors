0c897c46771ecf8d221ea783797aaf9b1c965bd4
DarioLoForti <lofortidario@gmail.com>
2024-03-28 16:21:44 +0100

    form messaggi

---
+++ resources/views/admin/messages/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Inserisci il tuo messaggio</h3>
+            </div>
+            <div class="col-12">
+                <form action="{{ route('message.store') }}" enctype="multipart/form-data" method="POST">
+                    @csrf
+                    <div class="form-group mb-3">
+                        <label for="name">Nome</label>
+                        <input type="text" name="nome" id="nome"class="form-control"
+                            placeholder="Inserisci il tuo nome">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email"class="form-control"
+                            placeholder="Inserisci la tua E-mail">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="name">Testo</label>
+                        <textarea name="text" id="text" cols="30" rows="10"></textarea>
+                    </div>
+                    <div class="form-group my-3">
+                        <button class="btn btn-success" type="submit">Salva</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
