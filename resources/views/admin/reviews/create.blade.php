557baacb88b52d25627b4553f196e4f91c8fc70a
Gresdian <chris_da98@outlook.it>
2024-03-28 16:33:33 +0100

    Create review

---
+++ resources/views/admin/reviews/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.style')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Aggiungi una recensione</h2>
+            </div>
+            <div class="col-12">
+                <form action="{{ route('review.store') }}" enctype="multipart/form-data" method="POST">
+                    @csrf
+                    <div class="form-group my-2">
+                        <label for="name">Nome</label>
+                        <input type="text" id="nome" name="nome" class="form-control">
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="email">E-mail</label>
+                        <input type="email" id="e-mail" name="e-mail" class="form-control">
+                    </div>
+                    <div class="form-group my-2">
+                        <label for="review">Inserisci recensione</label>
+                        <textarea class="form-control" id='review' name="review" cols="30" row="10">
+                        </div>
+                        <button type="submit" class="btn btn-success ">Invia</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+@endsection
