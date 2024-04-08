52e5f9321a07ce35bf6ba9e60a78b63d5296efe8
nicomanunta <nic.manun@gmail.com>
2024-04-08 12:55:05 +0200

    pagina pagamento

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -28,28 +28,46 @@
         .braintree-upper-container:before {
             background-color: white
         }
+        .braintree-upper-container{
+            z-index: unset;
+        }
+        .braintree-card{
+            position: relative;
+            border-radius: 20px
+
+        }
+        .position-img{
+          
+        }
+        
+
     </style>
 
     <body>
-        <div class="container mt-5">
-            <div class="card text-center p-4">
-                <div class="mb-3">
-                    <h4 class="mb-3">Conferma Pagamento</h4>
-                    <p>Si sta procedendo al pagamento di <strong>{{ $sponsorship->price }}</strong> euro per il servizio
-                        <strong>{{ $sponsorship->name }}</strong>, della durata di
-                        <strong>{{ $sponsorship->duration }}</strong> ore.
-                    </p>
+        <div class="bg-linear-gradient">
+            <div class="container pt-5 ">
+                <div class="card text-center py-3">
+                    <div class="mb-3">
+                        <h4 class="mb-3">Conferma Pagamento</h4>
+                        <p>Si sta procedendo al pagamento di <strong>{{ $sponsorship->price }}</strong> euro per il servizio
+                            <strong>{{ $sponsorship->name }}</strong>, della durata di
+                            <strong>{{ $sponsorship->duration }}</strong> ore.
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="py-4 text-center">
-                @csrf
-                <div id="dropin-container"></div>
-                <div>
-                    <button id="submit-button" class="btn btn-success">Paga</button>
-                    <a href="javascript:history.back()" class="btn btn-secondary ms-2">Indietro</a>
-                </div>
-                <div id="success-message" class="alert alert-success mt-3" style="display: none;">
-                    Pagamento avvenuto con successo!
+                <div class="py-4 text-center">
+                    {{-- <div class="position-img">
+                        <img src="{{ Vite::asset('resources/img/carta.png') }}" alt="">
+                    </div> --}}
+                    @csrf
+                    <div id="dropin-container"></div>
+                    <div>
+                        <button id="submit-button" class="btn btn-success">Paga</button>
+                        <a href="javascript:history.back()" class="btn btn-secondary ms-2">Indietro</a>
+                    </div>
+                    <div id="success-message" class="alert alert-success mt-3" style="display: none;">
+                        Pagamento avvenuto con successo!
+                    </div>
                 </div>
             </div>
         </div>
